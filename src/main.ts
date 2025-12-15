77fdabaa0c2316728d265493be3571df468bca1a
Lucas Kesselman <lucaskesselman@gmail.com>
2025-12-15 09:18:03 -0800

    removed Zappar for testing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Engine from './core/Engine.ts';
 import LevelManager from './core/LevelManager.ts';
+import * as ZapparBabylon from '@zappar/zappar-babylonjs';
+import * as MAT from '@babylonjs/materials';
 
 
 
@@ -29,17 +31,109 @@ function init(): void {
     // initEngine();
     // initLevelManager();
     
+
+
+    // new code to set up basic babyon scene here
+
+
+    const target = './assets/computer-vision-assets/target1.png';
+
+    const canvas = document.getElementById("canvas-background") as HTMLCanvasElement
+    const engine = new BABYLON.Engine(canvas, true)
+
+
+    const createScene = async () => {
+      const scene = new BABYLON.Scene(engine)
+
+      const camera = new BABYLON.FreeCamera(
+        "camera",
+        new BABYLON.Vector3(0, 1.6, 0),
+        scene
+      )
+      camera.setTarget(BABYLON.Vector3.Zero())
+
+      const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+      )
+
+      const box = BABYLON.MeshBuilder.CreateBox(
+        "trackedBox",
+        { size: 0.1 },
+        scene
+      )
+      box.isVisible = false
+
+      const xr = await scene.createDefaultXRExperienceAsync({
+        uiOptions: {
+          sessionMode: "immersive-ar"
+        },
+        optionalFeatures: true
+      })
+
+      const imageTracking = xr.baseExperience.featuresManager.enableFeature(
+        BABYLON.WebXRFeatureName.IMAGE_TRACKING,
+        "latest",
+        {
+          images: [
+            {
+              src: "./assets/computer-vision-assets/target1.png",
+              estimatedRealWorldWidth: 0.2
+            }
+          ]
+        }
+      )
+
+      imageTracking.onTrackedImageUpdatedObservable.add((image) => {
+        if (image.trackingState === "tracked") {
+          box.isVisible = true
+
+          image.transformationMatrix.decompose(
+            box.scaling,
+            box.rotationQuaternion,
+            box.position
+          )
+        } else {
+          box.isVisible = false
+        }
+      })
+
+      return scene
+    }
+
+    createScene().then(scene => {
+      engine.runRenderLoop(() => {
+        scene.render()
+      })
+    })
+
+    window.addEventListener("resize", () => {
+      engine.resize()
+    })
+
+
+    createScene().then(scene => {
+      engine.runRenderLoop(() => {
+        scene.render()
+      })
+    })
+
+
+    // start engine and
+    
+
     // create the game engine
-    let engine = new Engine();
+    //let engine = new Engine();
     
     // create the game level manager
-    let levelManager = new LevelManager();
+    //let levelManager = new LevelManager();
 
     // Load the first level
-    levelManager.LoadLevel(1, engine);
+    //levelManager.LoadLevel(1, engine);
 
     // start the game engine
-    engine.Start(1280, 720, levelManager);
+    //engine.Start(1280, 720, levelManager);
         
 }
     
